ff532cc1a248f895f48b79e66df846b1d0f8a238
Emanuel Machado <emanuel@emachado.pt>
2023-08-08 15:29:37 +0100

    Implemented full counter

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -11,7 +11,7 @@
       </span>
 
       <div v-else>
-        <h3 class="text-96">{{ counter }}</h3>
+        <h3 class="text-192" :class="`text-${counterColour}`">{{ counter }}</h3>
       </div>
     </div>
   </main>
@@ -20,28 +20,61 @@
 <script lang="ts">
 import Vue from 'vue'
 
+const counterColours = {
+  exercise: 'primary',
+  rest: 'secondary',
+  restRound: 'tertiary',
+}
+
 export default Vue.extend({
   name: 'MainComponent',
   data() {
     return {
       workoutStarted: false,
-      exerciseDuration: 60,
-      restDuration: 60,
+      exerciseDuration: 5,
+      restDuration: 3,
       rounds: 3,
-      restBetweenRounds: 180,
+      restBetweenRounds: 10,
       counter: 0,
+      counterColour: counterColours.exercise,
     }
   },
   methods: {
-    startWorkout() {
+    async startWorkout() {
       this.workoutStarted = true
-      this.counter = this.exerciseDuration
 
-      for (let i = 1; i <= this.exerciseDuration; i += 1) {
-        setTimeout(() => {
+      // For each round
+      for (let round = 1; round <= this.rounds; round += 1) {
+        // Exercise
+        this.counter = this.exerciseDuration
+        this.counterColour = counterColours.exercise
+
+        for (let second = 1; second <= this.exerciseDuration; second += 1) {
+          await this.sleep(1000)
           this.counter -= 1
-        }, i * 1000)
+        }
+
+        // Rest
+        this.counter = this.restDuration
+        this.counterColour = counterColours.rest
+
+        for (let second = 1; second <= this.restDuration; second += 1) {
+          await this.sleep(1000)
+          this.counter -= 1
+        }
+
+        // Rest between rounds
+        this.counter = this.restBetweenRounds
+        this.counterColour = counterColours.restRound
+
+        for (let second = 1; second <= this.restBetweenRounds; second += 1) {
+          await this.sleep(1000)
+          this.counter -= 1
+        }
       }
+    },
+    async sleep(ms: number) {
+      return new Promise((resolve) => setTimeout(resolve, ms))
     },
   },
 })
