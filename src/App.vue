de3e3e10686215755bafb7643337a0b14e791d9e
Emanuel Machado <emanuel@emachado.pt>
2023-08-08 11:39:29 +0100

    Setup

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Welcome to the Spartacus Workout Counter</h1>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Vue from 'vue'
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld,
   },
-});
+  created() {
+    document.title = 'Spartacus Workout | Counter'
+  },
+})
 </script>
 
 <style lang="scss">
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
