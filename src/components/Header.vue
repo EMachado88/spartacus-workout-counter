22c589e404452b6cf8913ca569f8de2ed41929c7
Emanuel Machado <emanuel@emachado.pt>
2023-08-08 16:07:37 +0100

    Fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,8 @@
 <template>
-<<<<<<< HEAD
   <header class="header d-flex justify-space-between">
     <a class="logo" href="javascript:void(0)">
       <img class="w-100" src="img/icons/safari-pinned-tab.svg" alt="Spartacus Workout counter">
     </a>
-=======
-  <header class="header justify-center">
-    <h2 class="ma-0">
-      <a class="logo" href="javascript:void(0)">
-        <img class="w-100" src="img/icons/safari-pinned-tab.svg" alt="Spartacus Workout counter">
-      </a>
-    </h2>
->>>>>>> adde2e9243d76ea1c94c9f5bb36c584f0641c8d3
   </header>
 </template>
 
