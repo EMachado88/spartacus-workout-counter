da0a6bbcbcc779c20bcde00480fe444c3de396db
Emanuel Machado <emanuel@emachado.pt>
2023-08-08 15:29:06 +0100

    ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     semi: 0,
+    'no-promise-executor-return': 'off',
+    'no-await-in-loop': 'off',
   },
   overrides: [
     {
