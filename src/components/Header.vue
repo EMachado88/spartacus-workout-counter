80c88380857c4a9578bc1bc6a8f7aab86734b461
Emanuel Machado <emanuel@emachado.pt>
2023-08-08 16:04:13 +0100

    Logo in the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
-  <header class="header justify-center">
-    <h2 class="ma-0 text-center">
-      <span class="text-secondary">Spartacus Workout</span> Counter
-    </h2>
+  <header class="header d-flex justify-space-between">
+    <a class="logo" href="javascript:void(0)">
+      <img class="w-100" src="img/icons/safari-pinned-tab.svg" alt="Spartacus Workout counter">
+    </a>
   </header>
 </template>
 
@@ -13,3 +13,10 @@ export default Vue.extend({
   name: 'HeaderComponent',
 })
 </script>
+
+<style lang="scss">
+.logo {
+  display: block;
+  max-width: 60px;
+}
+</style>
