10fc524fb6d82bc411f273f8fbc4516cd2426e72
Emanuel Machado <emanuel@emachado.pt>
2023-08-08 12:12:26 +0100

    Icons and variables

--- src/App.vue
+++ src/App.vue
@@ -18,18 +18,17 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
-html, body {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  height: 100%;
-}
+@import './assets/scss/main';
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  display: flex;
+  color: $white;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
 }
 </style>
